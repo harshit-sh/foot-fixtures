e16fa628e37f932506a41505445d4fefc31115e6
Harshit Sharma <hsharma1205@gmail.com>
2014-07-29 10:58:28 +0530

    first commit

---
+++ codes.py
@@ -0,0 +1,23 @@
+code_dict1 = {'Arsenal' : 'ARS','Aston Villa': 'AVL','Burnley': 'BUR','Chelsea': 'CHE','Crystal Palace': 'CRY',
+	'Everton': 'EVE','Hull City':'HUL','Leicester City':'LEI','Liverpool': 'LIV','Manchester City':'MCI',
+	'Manchester United':'MUN','Newcastle United':'NEW','Queens Park Rangers':'QPR',
+	'Southampton':'SOT','Stoke City':'STK','Sunderland':'SUN','Swansea City':'SWA',
+	'Tottenham Hotspur':'TOT','West Bromwich Albion':'WBA','West Ham United':'WHU',
+	
+	'Almeria':'ALM','Athletic Bilbao':'ATB','Atletico Madrid':'ATM','Barcelona':'BAR',
+	'Celta de Vigo':'CDV','Cordoba':'COR','Deportivo':'DEP','Eibar':'EIB','Elche':'ELC',
+	'Espanyol':'ESP','Getafe':'GET','Granada':'GRA','Levante':'LEV','Malaga':'MAL',
+	'Rayo Vallecano':'RAV','Real Madrid':'RMA','Real Sociedad':'RSO','Sevilla':'SEV',
+	'Valencia':'VAL','Villarreal':'VIL'}
+
+#inv_map = {v:k for k, v in map.items()}
+
+code_dict2 = {'COR': 'Cordoba', 'ATB': 'Athletic Bilbao', 'DEP': 'Deportivo', 'SUN': 'Sunderland', 
+	'CRY': 'Crystal Palace', 'EVE': 'Everton', 'TOT': 'Tottenham Hotspur', 'LIV': 'Liverpool', 
+	'EIB': 'Eibar', 'WBA': 'West Bromwich Albion', 'BAR': 'Barcelona', 'MUN': 'Manchester United', 
+	'SEV': 'Sevilla', 'GET': 'Getafe', 'HUL': 'Hull City', 'NEW': 'Newcastle United', 'RAV': 'Rayo Vallecano', 
+	'SWA': 'Swansea City', 'RMA': 'Real Madrid', 'SOT': 'Southampton', 'QPR': 'Queens Park Rangers', 
+	'ATM': 'Atletico Madrid', 'CHE': 'Chelsea', 'VAL': 'Valencia', 'ESP': 'Espanyol', 'ELC': 'Elche', 
+	'WHU': 'West Ham United', 'STK': 'Stoke City', 'ARS': 'Arsenal', 'GRA': 'Granada', 'RSO': 'Real Sociedad', 
+	'MCI': 'Manchester City', 'LEI': 'Leicester City', 'ALM': 'Almeria', 'VIL': 'Villarreal', 'AVL': 'Aston Villa',
+	'BUR': 'Burnley', 'CDV': 'Celta de Vigo', 'LEV': 'Levante', 'MAL': 'Malaga'}

55395adcc41ced75f72b9b4787901241751d5521
Harshit Sharma <hsharma1205@gmail.com>
2014-07-29 13:54:03 +0530

    changed setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name='foot-fixtures',
     version='1.0.0',
     author_email='hsharma1205@gmail.com',
-	py_modules=['fixtures'],
+	py_modules=['fixtures','teams_prac','codes'],
     entry_points={
         'console_scripts': [
-            'fixtures = fixtures:main',
+            'foot-fixtures = fixtures:main',
         ],
     },
     url = 'https://github.com/supercr7/foot-fixtures',
-	download_url = 'https://github.com/supercr7/foot-fixtures/tarball/1.3',
+	download_url = 'https://github.com/supercr7/foot-fixtures/tarball/1.4',
     install_requires=[
         'colorama==0.3.1'
     ],
